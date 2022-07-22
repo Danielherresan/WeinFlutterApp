304240e007103a13e6849e68b5f13067b3ad083c
daniherr <daniherr@bancolombia.com.co>
2022-07-21 20:39:22 -0500

    subiendo cambios

--- wein_flutter_app/lib/main.dart
+++ wein_flutter_app/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:wein_flutter_app/app.dart';
 
-void main() {
-  runApp(const WeinFlutterApp());
-}
+void main()  => runApp(const WeinFlutterApp());
+
 
